d371eeaa7311dce0c3e2a7874e72a4088f5551e2
vilera <v.vasikova.6@gmail.com>
2023-01-14 16:20:59 +0500

    fix bug

--- vilerasite/asgi.py
+++ vilerasite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vileravas project.
+ASGI config for vilerasite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vileravas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vilerasite.settings')
 
 application = get_asgi_application()
